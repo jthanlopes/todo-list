309eb3a04f333ac2f3922c4119d750c39f469604
Jonathan dos Santos <jthan.lopes@gmail.com>
2023-01-06 10:49:07 -0300

    feat: criação das funcionalidades do todo list

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -1,13 +1,77 @@
-import styles from './Tasks.module.css';
+import { useState } from 'react';
 import { PlusCircle, Trash } from 'phosphor-react';
+import { v4 as uuidv4 } from 'uuid';
+
+import styles from './Tasks.module.css';
+
+interface TaskProps {
+  id: string;
+  content: string;
+  isCompleted: boolean;
+}
 
 export function Tasks() {
+  const [tasks, setTasks] = useState<TaskProps[]>([
+    {
+      id: uuidv4(),
+      content: 'Tarefa 1',
+      isCompleted: false
+    },
+    {
+      id: uuidv4(),
+      content: 'Tarefa 2',
+      isCompleted: true
+    },
+    {
+      id: uuidv4(),
+      content: 'Tarefa 3',
+      isCompleted: true
+    }
+  ]);
+  const [taskContent, setTaskContent] = useState('');
+  const isNewTaskEmpty = taskContent.length === 0;
+
+  function handleAddTask() {
+    if(isNewTaskEmpty) return
+
+    const newTask: TaskProps = {
+      id: uuidv4(),
+      content: taskContent,
+      isCompleted: false
+    }
+
+    setTasks((state) => [...state, newTask]);
+    setTaskContent('');
+  }
+
+  function handleCompleteTask(idTask: string) {
+    const newTasks = tasks.map(task => task.id === idTask ? {
+      ...task,
+      isCompleted: !task.isCompleted
+    }: task);
+
+    setTasks(newTasks);
+  }
+
+  function handleDeleteTask(idTask: string) {
+    const filteredTasks = tasks.filter(task => task.id !== idTask);
+
+    setTasks(filteredTasks);
+  }
+
   return (
     <main className={styles.tasks}>
       <div className={styles.addTasks}>
-        <input type="text" placeholder="Adicione uma nova tarefa"/>
+        <input
+          type="text"
+          placeholder="Adicione uma nova tarefa"
+          onChange={(event) => setTaskContent(event.target.value)}
+          value={taskContent}
+        />
 
-        <button>
+        <button
+          onClick={handleAddTask}
+        >
           Criar
           <PlusCircle size={16}/>
         </button>
@@ -20,13 +84,19 @@ export function Tasks() {
         </header>
 
         <ul>
-          <li>
-            <button>v</button>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, fuga.</p>
-            <button title="Deletar tarefa">
-              <Trash />
-            </button>
-          </li>
+          {
+            tasks.map(task => {
+              return (
+                <li key={task.id}>
+                  <button onClick={() => handleCompleteTask(task.id)}>{task.isCompleted.toString()}</button>
+                  <p>{task.content}</p>
+                  <button title="Deletar tarefa" onClick={() => handleDeleteTask(task.id)}>
+                    <Trash />
+                  </button>
+                </li>
+              )
+            })
+          }
         </ul>
       </section>
     </main>
